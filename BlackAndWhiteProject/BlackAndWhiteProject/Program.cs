4cadb6950b78ee5520a3abfbe7f7f7b5522dc97a
black <black@example.com>
2022-10-25 18:51:19 +0300

    Initial commit

---
+++ BlackAndWhiteProject/BlackAndWhiteProject/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace BlackProject
+{
+    static class Black
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, I'm Black!");
+        }
+    }
+}
